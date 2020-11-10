9ed9f097738bfda4c42e45b0783bf36edd88059c
Julian Moorhouse <julesthecoder@gmail.com>
2020-11-10 19:33:04 +0000

    Initial files for challenge X01

---
+++ X01 Unit Conversion/X01 Unit Conversion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  X01 Unit Conversion
+//
+//  Created by Julian Moorhouse on 10/11/2020.
+//  Copyright © 2020 Mindwarp Consultancy Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
