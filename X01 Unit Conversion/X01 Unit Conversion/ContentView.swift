2affa70e37bacdb7f814e3ec2855c3fa5131b460
Julian Moorhouse <julesthecoder@gmail.com>
2020-11-10 21:17:00 +0000

    Completed challenge

--- X01 Unit Conversion/X01 Unit Conversion/ContentView.swift
+++ X01 Unit Conversion/X01 Unit Conversion/ContentView.swift
@@ -9,8 +9,82 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var input = ""
+    @State private var inputUnit = 0
+    @State private var outputUnit = 0
+    
+    var inputMeters: Measurement<UnitLength> {
+        let number: Double = Double(input) ?? 0
+        switch unitTypes[inputUnit] {
+            case "Meters":
+                return Measurement(value: number, unit: UnitLength.meters)
+                    .converted(to: .meters)
+            case "Kilometers":
+                return Measurement(value: number, unit: UnitLength.kilometers)
+                    .converted(to: .meters)
+            case "Feet":
+                return Measurement(value: number, unit: UnitLength.feet)
+                    .converted(to: .meters)
+            case "Yards":
+                return Measurement(value: number, unit: UnitLength.yards)
+                    .converted(to: .meters)
+            case "Miles":
+                return Measurement(value: number, unit: UnitLength.miles)
+                    .converted(to: .meters)
+            default:
+                return Measurement(value: 0, unit: UnitLength.meters)
+                    .converted(to: .meters)
+        }
+    }
+
+    var conversionOutput: Measurement<UnitLength> {
+        switch unitTypes[outputUnit] {
+            case "Meters":
+                return inputMeters.converted(to: .meters)
+            case "Kilometers":
+                return inputMeters.converted(to: .kilometers)
+            case "Feet":
+                return inputMeters.converted(to: .feet)
+            case "Yards":
+                return inputMeters.converted(to: .yards)
+            case "Miles":
+                return inputMeters.converted(to: .miles)
+            default:
+                return inputMeters.converted(to: .meters)
+        }
+    }
+
+    let unitTypes = ["Meters", "Kilometers", "Feet", "Yards", "Miles"]
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section(header: Text("Input unit")) {
+                    Picker("From ", selection: $inputUnit) {
+                        ForEach(0 ..< unitTypes.count) {
+                            Text("\(self.unitTypes[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Output unit")) {
+                    Picker("To", selection: $outputUnit) {
+                        ForEach(0 ..< unitTypes.count) {
+                            Text("\(self.unitTypes[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section {
+                    TextField("Amount", text: $input)
+                    .keyboardType(.decimalPad)
+                }
+                Section(header: Text("Conversion")) {
+                    Text("\(conversionOutput.description)")
+                }
+            }
+            .navigationBarTitle("Unit Conversion")
+        }
     }
 }
 
