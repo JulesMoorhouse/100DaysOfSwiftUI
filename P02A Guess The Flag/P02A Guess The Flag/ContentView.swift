eb961200016e9a3856bd45f0f17b8425b5d6b825
Julian Moorhouse <julesthecoder@gmail.com>
2020-11-14 12:54:02 +0000

    Initial files for the Guess the flag project

---
+++ P02A Guess The Flag/P02A Guess The Flag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P02A Guess The Flag
+//
+//  Created by Julian Moorhouse on 14/11/2020.
+//  Copyright © 2020 Mindwarp Consultancy Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
