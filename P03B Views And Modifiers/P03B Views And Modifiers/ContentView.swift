57fabe508304c46d73001d40d9a28023f5150b05
Julian Moorhouse <julesthecoder@gmail.com>
2020-11-17 20:39:15 +0000

    Custom modifier examples

--- P03B Views And Modifiers/P03B Views And Modifiers/ContentView.swift
+++ P03B Views And Modifiers/P03B Views And Modifiers/ContentView.swift
@@ -8,9 +8,75 @@
 
 import SwiftUI
 
+struct CapsuleText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .background(Color.blue)
+            .clipShape(Capsule())
+    }
+}
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(Color.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        self.modifier(Watermark(text: text))
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        //        VStack(spacing: 10) {
+        //            CapsuleText(text: "First")
+        //                .foregroundColor(.white)
+        //
+        //            CapsuleText(text: "Second")
+        //                .foregroundColor(.yellow)
+        //        }
+        // ---
+        //    Text("Hello World!")
+        //        .titleStyle()
+        //        //.modifier(Title())
+        //    }
+        // ---
+        Color.blue
+            .frame(width: 300, height: 300)
+            .watermarked(with: "Hacking with Swift")
     }
 }
 
