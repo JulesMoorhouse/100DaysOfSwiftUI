711599cc4f7fa03f576586b33cf27c4c7da0185e
Julian Moorhouse <julesthecoder@gmail.com>
2020-11-17 20:01:18 +0000

    Initial files for additional views and modifier examples

---
+++ P03B Views And Modifiers/P03B Views And Modifiers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P03B Views And Modifiers
+//
+//  Created by Julian Moorhouse on 17/11/2020.
+//  Copyright © 2020 Mindwarp Consultancy Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
