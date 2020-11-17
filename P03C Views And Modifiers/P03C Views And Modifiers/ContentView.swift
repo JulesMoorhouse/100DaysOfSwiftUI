bfa54a93dea060c7714f1a64aeac3be26e4fa463
Julian Moorhouse <julesthecoder@gmail.com>
2020-11-17 20:53:17 +0000

    Custom containers

---
+++ P03C Views And Modifiers/P03C Views And Modifiers/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  P03C Views And Modifiers
+//
+//  Created by Julian Moorhouse on 17/11/2020.
+//  Copyright © 2020 Mindwarp Consultancy Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+    
+    var body: some View {
+        VStack {
+            ForEach(0 ..< rows) { row in
+                HStack {
+                    ForEach(0 ..< self.columns) { column in
+                        self.content(row, column)
+                    }
+                }
+            }
+        }
+    }
+    
+    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
+        self.rows = rows
+        self.columns = columns
+        self.content = content
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        GridStack(rows: 4, columns: 4) { row, col in
+            HStack {
+                Image(systemName: "\(row * 4 + col).circle")
+                Text("R\(row) C\(col)")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
