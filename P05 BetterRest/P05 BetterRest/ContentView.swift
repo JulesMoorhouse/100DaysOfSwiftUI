e58ef234f63b756a3365f9313f9cd98fbd81bb9c
Julian Moorhouse <julesthecoder@gmail.com>
2020-12-03 21:10:40 +0000

    Selecting date and times with date picker

--- P05 BetterRest/P05 BetterRest/ContentView.swift
+++ P05 BetterRest/P05 BetterRest/ContentView.swift
@@ -9,8 +9,31 @@
 import SwiftUI
 
 struct ContentView: View {
+//    @State private var sleepAmount = 8.0
+//
+//    var body: some View {
+//        Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+//            Text("\(sleepAmount, specifier: "%g") hours")
+//        }
+//    }
+// ---
+    @State private var wakeUp = Date()
+    
     var body: some View {
-        Text("Hello, World!")
+//        Form {
+//            DatePicker("Please enter a date", selection: $wakeUp)
+//        }
+// ---
+//        let now = Date()
+//        let tomorrow = Date().addingTimeInterval(86400)
+//        let range = now ... tomorrow
+//
+//        DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
+//            .labelsHidden()
+// ---
+        // Allow all dates in the future
+        DatePicker("Please enter a date", selection: $wakeUp, in: Date()...)
+            .labelsHidden()
     }
 }
 
