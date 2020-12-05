8b02a6a822cecd3f3a29e3b509ba87134e3e5d25
Julian Moorhouse <julesthecoder@gmail.com>
2020-12-05 10:23:56 +0000

    DatePicker updates and basic ML file

--- P05 BetterRest/P05 BetterRest/ContentView.swift
+++ P05 BetterRest/P05 BetterRest/ContentView.swift
@@ -9,30 +9,50 @@
 import SwiftUI
 
 struct ContentView: View {
-//    @State private var sleepAmount = 8.0
-//
-//    var body: some View {
-//        Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-//            Text("\(sleepAmount, specifier: "%g") hours")
-//        }
-//    }
-// ---
+    //    @State private var sleepAmount = 8.0
+    //
+    //    var body: some View {
+    //        Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+    //            Text("\(sleepAmount, specifier: "%g") hours")
+    //        }
+    //    }
+    // ---
+    //    @State private var wakeUp = Date()
+    //
+    //    var body: some View {
+    //        Form {
+    //            DatePicker("Please enter a date", selection: $wakeUp)
+    //        }
+    // ---
+    //        let now = Date()
+    //        let tomorrow = Date().addingTimeInterval(86400)
+    //        let range = now ... tomorrow
+    //
+    //        DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
+    //            .labelsHidden()
+    // ---
+    //        // Allow all dates in the future
+    //        DatePicker("Please enter a date", selection: $wakeUp, in: Date()...)
+    //            .labelsHidden()
+    // ---
     @State private var wakeUp = Date()
     
     var body: some View {
-//        Form {
-//            DatePicker("Please enter a date", selection: $wakeUp)
-//        }
-// ---
-//        let now = Date()
-//        let tomorrow = Date().addingTimeInterval(86400)
-//        let range = now ... tomorrow
+//        var components = DateComponents()
+//        components.hour = 8
+//        components.minute = 0
 //
-//        DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
-//            .labelsHidden()
-// ---
-        // Allow all dates in the future
-        DatePicker("Please enter a date", selection: $wakeUp, in: Date()...)
+//        let date = Calendar.current.date(from: components) ?? Date()
+//---
+//        let components = Calendar.current.date(from: [.hour, .minute], from: someDate)
+//        let hour = components.hour ?? 0
+//        let minute = components.minute ?? 0
+        // ---
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        let dateString = formatter.string(from: Date())
+        
+        return DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
             .labelsHidden()
     }
 }
