583d43bbd24e8cf1e26129a9578e64dbe11fdf21
Julian Moorhouse <julesthecoder@gmail.com>
2020-12-05 11:00:51 +0000

    Better rest basic layout

--- P05C BetterRest/P05C BetterRest/ContentView.swift
+++ P05C BetterRest/P05C BetterRest/ContentView.swift
@@ -9,8 +9,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUp = Date()
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                    Text("\(sleepAmount, specifier: "%g")")
+                }
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper(value: $coffeeAmount, in: 1...20) {
+                    if coffeeAmount == 1 {
+                        Text("1 cup")
+                    } else {
+                        Text("\(coffeeAmount) cups")
+                    }
+                }
+            }
+            .navigationBarTitle("BetterRest")
+            .navigationBarItems(trailing:
+                Button(action: calculateBedtime) {
+                    Text("Calculate")
+                }
+            )
+        }
+    }
+    
+    func calculateBedtime() {
+        
     }
 }
 
