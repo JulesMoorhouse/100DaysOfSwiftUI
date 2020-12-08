d8a66352c859f515c5f77a13c47f5c5b91c44db0
Julian Moorhouse <julesthecoder@gmail.com>
2020-12-08 20:56:57 +0000

    Initial files for animation project

---
+++ P07 Animations/P07 Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P07 Animations
+//
+//  Created by Julian Moorhouse on 08/12/2020.
+//  Copyright © 2020 Mindwarp Consultancy Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
