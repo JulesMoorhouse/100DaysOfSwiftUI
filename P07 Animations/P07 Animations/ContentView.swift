3f7dd0b02057c731a6fb577d2127db5b1f9a3062
Julian Moorhouse <julesthecoder@gmail.com>
2020-12-09 19:25:44 +0000

    Customizing animations

--- P07 Animations/P07 Animations/ContentView.swift
+++ P07 Animations/P07 Animations/ContentView.swift
@@ -12,6 +12,32 @@ struct ContentView: View {
     @State private var animationAmount: CGFloat = 1
     
     var body: some View {
+//        Button("Tap me") {
+//            self.animationAmount += 1
+//        }
+//        .padding(50)
+//        .background(Color.red)
+//        .foregroundColor(.white)
+//        .clipShape(Circle())
+//        .scaleEffect(animationAmount)
+//        .blur(radius: (animationAmount - 1) * 3)
+//        //.animation(.default)
+//        //.animation(.easeOut)
+//        //.animation(.interpolatingSpring(stiffness: 50, damping: 1))
+//        //.animation(.easeInOut(duration: 2))
+//        //.animation(
+//        //    Animation.easeInOut(duration: 2)
+//        //    .delay(1) //wait before doing animation
+//        //)
+//        //.animation(
+//        //    Animation.easeInOut(duration: 1)
+//        //        .repeatCount(3, autoreverses: true)
+//        //)
+//        .animation(
+//            Animation.easeInOut(duration: 1)
+//            .repeatForever(autoreverses: true)
+//        )
+        // ---
         Button("Tap me") {
             self.animationAmount += 1
         }
@@ -19,9 +45,20 @@ struct ContentView: View {
         .background(Color.red)
         .foregroundColor(.white)
         .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(.default)
+        .overlay(
+            Circle()
+                .stroke(Color.red)
+                .scaleEffect(animationAmount)
+                .opacity(Double(2 - animationAmount))
+                .animation(
+                    Animation.easeInOut(duration: 1)
+                    .repeatForever(autoreverses: false)
+                )
+        )
+        .onAppear  {
+            self.animationAmount = 2
+        }
+
     }
 }
 
