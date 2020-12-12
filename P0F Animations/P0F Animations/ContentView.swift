3a67e1b04d3d8ddf4e383083995dc50a06d99e59
Julian Moorhouse <julesthecoder@gmail.com>
2020-12-12 12:58:59 +0000

    Initial files for further animating gestures example.

---
+++ P0F Animations/P0F Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P0F Animations
+//
+//  Created by Julian Moorhouse on 12/12/2020.
+//  Copyright © 2020 Mindwarp Consultancy Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
