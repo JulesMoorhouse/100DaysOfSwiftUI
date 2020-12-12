ca427296a652a74c8315d5622a0ac9ad1d7e9dd7
Julian Moorhouse <julesthecoder@gmail.com>
2020-12-12 12:56:07 +0000

    Animating gestures example.

--- P07E Animations/P07E Animations/ContentView.swift
+++ P07E Animations/P07E Animations/ContentView.swift
@@ -9,8 +9,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var dragAmount = CGSize.zero
+    
     var body: some View {
-        Text("Hello, World!")
+        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged{self.dragAmount = $0.translation}
+                    //.onEnded{_ in self.dragAmount = .zero}
+                    // Explicit animation
+                    .onEnded{_ in
+                        withAnimation(.spring()) {
+                            self.dragAmount = .zero
+                        }
+                    }
+                
+        )
+        // Implicit animation
+        //.animation(.spring())
     }
 }
 
