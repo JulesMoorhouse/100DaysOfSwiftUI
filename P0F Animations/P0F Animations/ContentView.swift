41e884377740bb0c160f68c2d9bb2cad13af0456
Julian Moorhouse <julesthecoder@gmail.com>
2020-12-12 13:11:00 +0000

    Animating gestures example.

--- P0F Animations/P0F Animations/ContentView.swift
+++ P0F Animations/P0F Animations/ContentView.swift
@@ -9,8 +9,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    let letters = Array("Hello SwiftUI")
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
     var body: some View {
-        Text("Hello, World!")
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count) { num in
+                Text(String(self.letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(self.enabled ? Color.blue : Color.red)
+                    .offset(self.dragAmount)
+                    .animation(Animation.default.delay(Double(num) / 20))
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { self.dragAmount = $0.translation }
+                .onEnded { _ in
+                    self.dragAmount = .zero
+                    self.enabled.toggle()
+        })
     }
 }
 
