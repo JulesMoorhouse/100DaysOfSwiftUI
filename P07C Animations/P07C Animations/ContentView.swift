1b780cb7106d8e0f4be597183748dbed03627143
Julian Moorhouse <julesthecoder@gmail.com>
2020-12-12 11:29:22 +0000

    Explicit animations.

--- P07C Animations/P07C Animations/ContentView.swift
+++ P07C Animations/P07C Animations/ContentView.swift
@@ -9,8 +9,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 0.0
+    
     var body: some View {
-        Text("Hello, World!")
+        Button("Tap Me") {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                self.animationAmount += 360
+            }
+        }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(.degrees(animationAmount), axis:  (x: 0, y: 1, z: 0))
     }
 }
 
