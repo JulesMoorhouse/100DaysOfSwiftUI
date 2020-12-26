96caf3b7046e9ca20e247d85334f4216d5523711
Julian Moorhouse <julesthecoder@gmail.com>
2020-12-26 08:53:49 +0000

    Initial files for day 35 milestone project - times tables.

---
+++ X04 Times Tables/X04 Times Tables/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  X04 Times Tables
+//
+//  Created by Julian Moorhouse on 26/12/2020.
+//  Copyright © 2020 Mindwarp Consultancy Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
