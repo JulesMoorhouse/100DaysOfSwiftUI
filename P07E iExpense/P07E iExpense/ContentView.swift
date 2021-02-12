8d320c0457e0c2203d31d2d8cb8dd573aab07805
JulesMoorhouse <julesthecoder@gmail.com>
2021-02-12 14:13:26 +0000

    Day 36 wrap up.

---
+++ P07E iExpense/P07E iExpense/ContentView.swift
@@ -0,0 +1,92 @@
+//
+//  ContentView.swift
+//  P07E iExpense
+//
+//  Created by Julian Moorhouse on 12/02/2021.
+//
+
+import SwiftUI
+
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            let encoder = JSONEncoder()
+            
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "Items")
+        {
+            let decoder = JSONDecoder()
+            
+            if let decoded = try? decoder.decode([ExpenseItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+        
+        self.items = []
+    }
+}
+
+struct ContentView: View {
+    @ObservedObject var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(expenses.items) { item in
+                    //Text(item.name)
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        
+                        Text("$\(item.amount)")
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .navigationBarItems(trailing:
+                Button(action: {
+//                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+//                    self.expenses.items.append(expense)
+                    self.showingAddExpense = true
+                }) {
+                    Image(systemName: "plus")
+                }
+            )
+            .sheet(isPresented: $showingAddExpense) {
+                // Show an AddView here
+                AddView(expenses: self.expenses)
+            }
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
