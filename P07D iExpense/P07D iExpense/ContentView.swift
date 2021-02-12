d361a9a28e28f909d425b16ba1aea6479ed3dc02
JulesMoorhouse <julesthecoder@gmail.com>
2021-02-12 11:29:32 +0000

    Codable / JSONEncoder example.

---
+++ P07D iExpense/P07D iExpense/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  P07D iExpense
+//
+//  Created by Julian Moorhouse on 12/02/2021.
+//
+
+import SwiftUI
+
+struct User: Codable {
+    var firstName: String
+    var lastName: String
+}
+
+struct ContentView: View {
+    @State private var user = User(firstName: "Taylor", lastName: "Swift")
+    
+    var body: some View {
+        Button("Save User") {
+            let encoder = JSONEncoder()
+            
+            if let data = try? encoder.encode(self.user) {
+                UserDefaults.standard.set(data, forKey: "UserData")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P07D iExpense/P07D iExpense/P07D_iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  P07D_iExpenseApp.swift
+//  P07D iExpense
+//
+//  Created by Julian Moorhouse on 12/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct P07D_iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
