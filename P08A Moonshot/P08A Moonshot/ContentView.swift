b803340b3bce92e191cb9f9af8649ce879eb5879
Julian Moorhouse <julesthecoder@gmail.com>
2021-02-13 14:53:59 +0000

    Day 39 part 2.

---
+++ P08A Moonshot/P08A Moonshot/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  P08A Moonshot
+//
+//  Created by Julian Moorhouse on 13/02/2021.
+//
+
+import SwiftUI
+
+struct CustomText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(_ text: String) {
+        print("Creating a CustomText")
+        self.text = text
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        ScrollView(.vertical) {
+            VStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText("Item \($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P08A Moonshot/P08A Moonshot/P08A_MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  P08A_MoonshotApp.swift
+//  P08A Moonshot
+//
+//  Created by Julian Moorhouse on 13/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct P08A_MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
