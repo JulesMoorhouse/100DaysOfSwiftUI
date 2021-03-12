2028a4b5a087bc57f5ebbe189d57c075698d9ca0
JulesMoorhouse <julesthecoder@gmail.com>
2021-03-12 10:34:48 +0000

    Initial files for project 10.

---
+++ P10 CupcakeCorner/P10 CupcakeCorner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P10 CupcakeCorner
+//
+//  Created by Julian Moorhouse on 12/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P10 CupcakeCorner/P10 CupcakeCorner/P10_CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  P10_CupcakeCornerApp.swift
+//  P10 CupcakeCorner
+//
+//  Created by Julian Moorhouse on 12/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct P10_CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
