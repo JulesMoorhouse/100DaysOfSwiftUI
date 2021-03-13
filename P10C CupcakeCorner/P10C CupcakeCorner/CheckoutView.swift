51adf3e10b81cb3faa81502cae1c2f6be50fcdf8
Julian Moorhouse <julesthecoder@gmail.com>
2021-03-13 10:27:40 +0000

    Preparing for checkout .

--- P10C CupcakeCorner/P10C CupcakeCorner/CheckoutView.swift
+++ P10C CupcakeCorner/P10C CupcakeCorner/CheckoutView.swift
@@ -11,7 +11,25 @@ struct CheckoutView: View {
     @ObservedObject var order: Order
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            ScrollView {
+                VStack {
+                    Image("cupcakes")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width)
+                    
+                    Text("Your total is £\(self.order.cost, specifier: "%.2f")")
+                        .font(.title)
+                    
+                    Button("Place order") {
+                        // place the order
+                    }
+                    .padding()
+                }
+            }
+        }
+        .navigationBarTitle("Check out", displayMode: .inline)
     }
 }
 
