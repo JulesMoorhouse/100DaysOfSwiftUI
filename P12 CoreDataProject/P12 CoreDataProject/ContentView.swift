709e704dc4549a819fbe2eea9e85a5eace08e43f
Julian Moorhouse <julesthecoder@gmail.com>
2021-04-03 10:37:45 +0100

    Explanation of hasable and uniqueness.

--- P12 CoreDataProject/P12 CoreDataProject/ContentView.swift
+++ P12 CoreDataProject/P12 CoreDataProject/ContentView.swift
@@ -7,11 +7,15 @@
 
 import SwiftUI
 
+struct Student: Hashable {
+    let name: String
+}
+
 struct ContentView: View {
-
+    let students = [Student(name: "Harry Potter"), Student(name: "Hermione Granger")]
     var body: some View {
-        List {
-
+        List(students, id: \.self) { student in
+            Text(student.name)
         }
     }
 }
