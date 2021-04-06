da52a561a4df899de4a2f4d2ecb6614a13ca90fc
Julian Moorhouse <julesthecoder@gmail.com>
2021-04-06 18:33:57 +0100

    Day 60 challenge.

--- X06 Friends/X06 Friends/X06_FriendsApp.swift
+++ X06 Friends/X06 Friends/X06_FriendsApp.swift
@@ -9,12 +9,12 @@ import SwiftUI
 
 @main
 struct X06_FriendsApp: App {
-    let persistenceController = PersistenceController.shared
+    //let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                //.environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
