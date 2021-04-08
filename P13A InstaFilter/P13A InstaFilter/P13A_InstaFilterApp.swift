362b5c2547093686b8155d65582e1ff21a1dec6f
Julian Moorhouse <julesthecoder@gmail.com>
2021-04-08 10:37:31 +0100

    Initial files for P13A.

---
+++ P13A InstaFilter/P13A InstaFilter/P13A_InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  P13A_InstaFilterApp.swift
+//  P13A InstaFilter
+//
+//  Created by Julian Moorhouse on 08/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct P13A_InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
