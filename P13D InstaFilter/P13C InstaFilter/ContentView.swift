7222370b5a502f7921fd7d5ade94af8a54a0ff76
Julian Moorhouse <julesthecoder@gmail.com>
2021-04-09 16:24:17 +0100

    Basic copy of P13C to P13D.

---
+++ P13D InstaFilter/P13C InstaFilter/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  P13C InstaFilter
+//
+//  Created by Julian Moorhouse on 09/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var image: Image?
+    @State private var showingImagePicker = false
+    
+    var body: some View {
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+            
+            Button("Select Image") {
+                self.showingImagePicker = true
+            }
+        }
+        .sheet(isPresented: $showingImagePicker) {
+            ImagePicker()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P13D InstaFilter/README.md
@@ -0,0 +1,7 @@
+# Day 63 | [HWS 63](https://www.hackingwithswift.com/100/swiftui/63) | [Index](https://github.com/JulesMoorhouse/100DaysOfSwiftUI/blob/main/README.md)
+
+ - [P13C InstaFilter](https://github.com/JulesMoorhouse/100DaysOfSwiftUI/blob/main/P13C%20InstaFilter/P13C%20InstaFilter/ContentView.swift)
+ 
+ - Wrapping a UIViewController in a SwiftUI view.
+
+<img src="../Images/day63c.png" />
