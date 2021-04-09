9c0b94137a5b00696504af4f909573ad88d6e6ee
Julian Moorhouse <julesthecoder@gmail.com>
2021-04-09 15:58:47 +0100

    Initial files for P13C.

---
+++ P13C InstaFilter/P13C InstaFilter/P13C_InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  P13C_InstaFilterApp.swift
+//  P13C InstaFilter
+//
+//  Created by Julian Moorhouse on 09/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct P13C_InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
