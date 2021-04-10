7102bf692480e62aee997de3d7c9c45eccedfc38
Julian Moorhouse <julesthecoder@gmail.com>
2021-04-10 08:58:49 +0100

    Initial files for P13E.

---
+++ P13E InstaFilter/P13E InstaFilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P13E InstaFilter
+//
+//  Created by Julian Moorhouse on 10/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P13E InstaFilter/P13E InstaFilter/P13E_InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  P13E_InstaFilterApp.swift
+//  P13E InstaFilter
+//
+//  Created by Julian Moorhouse on 10/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct P13E_InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
