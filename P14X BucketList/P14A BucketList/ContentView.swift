466aef8ed8bcc7beadd388fef6cbb21b7fe11d93
Julian Moorhouse <julesthecoder@gmail.com>
2021-04-11 13:21:34 +0100

    Copy of P14A for P14X small challenge.

---
+++ P14X BucketList/P14A BucketList/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  P14A BucketList
+//
+//  Created by Julian Moorhouse on 11/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    var body: some View {
+        Text("Hello, world!")
+            .onTapGesture {
+                let str = "Test Message"
+                let url = self.getDocumentsDirectory().appendingPathComponent("message.txt")
+                
+                do {
+                    try str.write(to: url, atomically: true, encoding: .utf8)
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
