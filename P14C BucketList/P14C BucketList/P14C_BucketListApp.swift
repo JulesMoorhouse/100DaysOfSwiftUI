ca2428090e52b444c7344418763c62c6956ec7be
JulesMoorhouse <julesthecoder@gmail.com>
2021-04-22 08:12:11 +0100

    Initial files for project P14C.

---
+++ P14C BucketList/P14C BucketList/P14C_BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  P14C_BucketListApp.swift
+//  P14C BucketList
+//
+//  Created by Julian Moorhouse on 22/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct P14C_BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
