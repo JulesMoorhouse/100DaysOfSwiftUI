5011e9a1e25781c8090d4c744e3c6ab4b25d9405
JulesMoorhouse <julesthecoder@gmail.com>
2021-04-22 10:20:30 +0100

    Initial files for P14E.

---
+++ P14E BucketList/P14E BucketList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P14E BucketList
+//
+//  Created by Julian Moorhouse on 22/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P14E BucketList/README.md
@@ -0,0 +1,3 @@
+# Day 70 | [HWS 69](https://www.hackingwithswift.com/100/swiftui/70) | [Index](https://github.com/JulesMoorhouse/100DaysOfSwiftUI/blob/main/README.md)
+
+- [P14E BucketList](https://github.com/JulesMoorhouse/100DaysOfSwiftUI/blob/main/P14E%20BucketList/P14E%20BucketList/ContentView.swift) 
