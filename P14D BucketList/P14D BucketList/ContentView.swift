e19bb39cb01e8d974c86aaf911859e099e8d2b74
JulesMoorhouse <julesthecoder@gmail.com>
2021-04-22 09:48:21 +0100

    Using Touch ID and Face ID with SwiftUI

--- P14D BucketList/P14D BucketList/ContentView.swift
+++ P14D BucketList/P14D BucketList/ContentView.swift
@@ -5,12 +5,42 @@
 //  Created by Julian Moorhouse on 22/04/2021.
 //
 
+import LocalAuthentication
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isUnlocked = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            if self.isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data."
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, _ in
+                DispatchQueue.main.async {
+                    if success {
+                        self.isUnlocked = true
+                    } else {
+                        // there was a problem
+                    }
+                }
+            }
+        } else {
+            // no biometrics
+        }
     }
 }
 
