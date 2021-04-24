a4d2d720a0370e29017ddf6e9864d0a26bd8169b
Julian Moorhouse <julesthecoder@gmail.com>
2021-04-24 15:23:11 +0100

    Initial files for P15A.

---
+++ P15A Accessibility/P15A Accessibility/P15A_AccessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  P15A_AccessibilityApp.swift
+//  P15A Accessibility
+//
+//  Created by Julian Moorhouse on 24/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct P15A_AccessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
