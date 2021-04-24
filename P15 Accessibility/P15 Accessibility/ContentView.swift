a7880e6f9d9cce20999f6e0ab0a52c9e528d0f22
Julian Moorhouse <julesthecoder@gmail.com>
2021-04-24 15:00:54 +0100

    Identifying views with useful labels

--- P15 Accessibility/P15 Accessibility/ContentView.swift
+++ P15 Accessibility/P15 Accessibility/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+    
+    let labels = [
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks"
+    ]
+    
+    @State private var selectedPicture = Int.random(in: 0...3)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Image(pictures[selectedPicture])
+            .resizable()
+            .scaledToFit()
+            .accessibility(label: Text(labels[selectedPicture]))
+            .accessibility(addTraits: .isButton)
+            .accessibility(removeTraits: .isImage)
+            .onTapGesture {
+                self.selectedPicture = Int.random(in: 0...3)
+            }
     }
 }
 
