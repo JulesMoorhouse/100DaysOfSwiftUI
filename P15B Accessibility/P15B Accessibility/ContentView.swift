bb0f8022494eea5c9af99ce97aa9317cc5c946dd
Julian Moorhouse <julesthecoder@gmail.com>
2021-04-24 15:41:33 +0100

    Reading the value of controls.

--- P15B Accessibility/P15B Accessibility/ContentView.swift
+++ P15B Accessibility/P15B Accessibility/ContentView.swift
@@ -8,9 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    //@State private var estimate = 25.0
+    @State private var rating = 3
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        Slider(value: $estimate, in: 0...50)
+//            .padding()
+//            .accessibility(value: Text("\(Int(estimate))"))
+        Stepper("Rate our service: \(rating)/5", value: $rating, in: 1...5)
+            .accessibility(value: Text("\(rating) out of 5"))
     }
 }
 
