58f1c3b057aa72674a509c1e6c1a3d0eab25c4e3
Julian Moorhouse <julesthecoder@gmail.com>
2021-04-24 15:36:14 +0100

    Initial files for P15B.

---
+++ P15B Accessibility/P15B Accessibility/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P15B Accessibility
+//
+//  Created by Julian Moorhouse on 24/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P15B Accessibility/P15B Accessibility/P15B_AccessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  P15B_AccessibilityApp.swift
+//  P15B Accessibility
+//
+//  Created by Julian Moorhouse on 24/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct P15B_AccessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
