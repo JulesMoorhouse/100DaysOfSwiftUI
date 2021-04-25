1faa0b06a5fec41c16c111a65cd67246ef003b6b
Julian Moorhouse <julesthecoder@gmail.com>
2021-04-25 13:09:25 +0100

    Fixing Guess the Flag

--- P15X Guess The Flag/P02B Guess The Flag/ContentView.swift
+++ P15X Guess The Flag/P02B Guess The Flag/ContentView.swift
@@ -10,17 +10,48 @@ import SwiftUI
 
 struct FlagImage: View {
     var flagName: String
+    var labels: [String:String]
     var body: some View {
         Image(flagName)
             .renderingMode(.original)
             .clipShape(Capsule())
             .overlay(Capsule()
-            .stroke(Color.black, lineWidth: 1))
+                .stroke(Color.black, lineWidth: 1))
             .shadow(color: .black, radius: 2)
+            .accessibility(label: Text(self.labels[flagName] ?? "Unknown flag"))
+    }
+}
+
+struct AnswerView: View {
+    var country: String
+    var body: some View {
+        VStack {
+            Text("Tap the flag of")
+                .foregroundColor(.white)
+            
+            Text(country)
+                .foregroundColor(.white)
+                .font(.largeTitle)
+                .fontWeight(.black)
+        }
     }
 }
 
 struct ContentView: View {
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size, Left stripeblue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe is black, middle stripe red, bottom stripe yellow",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left strip green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top stripe red, middle stripe yellow, bottom stripe red. The middle stripe has a crest on the left",
+        "UK": "Flag with a red cross, two blue triangles in each corner and white inbetween",
+        "US": "Flag with a blue rectangle top left with white stars. The red and white horizontal stripes for the rest"
+    ]
+    
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
@@ -35,31 +66,22 @@ struct ContentView: View {
 
     var body: some View {
         ZStack {
-            //Color.blue.edgesIgnoringSafeArea(.all)
+            // Color.blue.edgesIgnoringSafeArea(.all)
             LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
             
-            VStack(spacing: 30 ) {
-                VStack {
-                    Text("Tap the flag of")
-                        .foregroundColor(.white)
-                    
-                    Text(countries[correctAnswer])
-                        .foregroundColor(.white)
-                        .font(.largeTitle)
-                        .fontWeight(.black)
-                }
-                
+            VStack(spacing: 30) {
+                AnswerView(country: countries[correctAnswer])
+
                 ForEach(0 ..< 3) { number in
                     Button(action: {
                         // flag was tapped
                         self.flagTapped(number)
                     }) {
-                        FlagImage(flagName: self.countries[number])
+                        FlagImage(flagName: self.countries[number], labels: self.labels)
                     }
                     .opacity(self.correctTapped && self.correctAnswer != number ? 1.0 : self.opacity)
-                    .rotation3DEffect(.degrees(self.correctTapped && self.correctAnswer == number ? 360 : 0), axis:  (x: 0, y: 1, z: 0))
+                    .rotation3DEffect(.degrees(self.correctTapped && self.correctAnswer == number ? 360 : 0), axis: (x: 0, y: 1, z: 0))
                     .scaleEffect(self.wrongTapped && self.correctAnswer == number ? 1.0 : self.scaleAmount)
-
                 }
                 
                 Text("Score: \(userScore)")
@@ -71,7 +93,7 @@ struct ContentView: View {
         .alert(isPresented: $showingScore) {
             Alert(title: Text(scoreTitle), message: Text(message), dismissButton: .default(Text("Continue")) {
                 self.askQuestion()
-                })
+            })
         }
     }
     
@@ -84,16 +106,15 @@ struct ContentView: View {
                 self.correctTapped = true
                 self.opacity -= 0.2
                 
-                
             } else {
                 scoreTitle = "Wrong"
-                message = ("Thats the flag of \(countries[number]).")
+                message = "Thats the flag of \(countries[number])."
                 self.wrongTapped = true
                 self.scaleAmount += 1
             }
         }
         
-        showingScore = true
+        self.showingScore = true
         self.correctTapped = false
         self.wrongTapped = false
         self.opacity = 1.0
@@ -101,8 +122,8 @@ struct ContentView: View {
     }
     
     func askQuestion() {
-        countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
+        self.countries.shuffle()
+        self.correctAnswer = Int.random(in: 0...2)
     }
 }
 
