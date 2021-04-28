4771f34c5f2ae8cb0cc8e2df6b6d4e13f92495d6
Julian Moorhouse <julesthecoder@gmail.com>
2021-04-28 17:16:31 +0100

    Renamed CoreData Entity.

---
+++ X07 Meetup/README.md
@@ -0,0 +1,1 @@
+# Day 77 | [HWS 77](https://www.hackingwithswift.com/100/swiftui/77) | [Index](https://github.com/JulesMoorhouse/100DaysOfSwiftUI/blob/main/README.md)
--- X07 Meetup/X07 Meetup/ContentView.swift
+++ X07 Meetup/X07 Meetup/ContentView.swift
@@ -12,9 +12,9 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        sortDescriptors: [NSSortDescriptor(keyPath: \Contacts.timestamp, ascending: true)],
         animation: .default)
-    private var items: FetchedResults<Item>
+    private var items: FetchedResults<Contacts>
 
     var body: some View {
         List {
@@ -36,7 +36,7 @@ struct ContentView: View {
 
     private func addItem() {
         withAnimation {
-            let newItem = Item(context: viewContext)
+            let newItem = Contacts(context: viewContext)
             newItem.timestamp = Date()
 
             do {
