6a09875e10e30908efadde643b00457502a3d464
Julian Moorhouse <julesthecoder@gmail.com>
2021-05-03 10:26:19 +0100

    Manually publishing ObservableObject changes.

--- P16D HotProspects/P16D HotProspects/ContentView.swift
+++ P16D HotProspects/P16D HotProspects/ContentView.swift
@@ -7,10 +7,29 @@
 
 import SwiftUI
 
+class DelayedUpdater: ObservableObject {
+    //@Published var value = 0
+    // Here we can do more when object change is published.
+    var value = 0 {
+        willSet {
+            objectWillChange.send()
+        }
+    }
+    
+    init() {
+        for i in 1...10 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.value += 1
+            }
+        }
+    }
+}
+
 struct ContentView: View {
+    @ObservedObject var updater = DelayedUpdater()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Value is: \(updater.value)")
     }
 }
 
