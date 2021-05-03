a86ddfac65a2b1819e965c1d4c19ab2f8a9b1cb1
Julian Moorhouse <julesthecoder@gmail.com>
2021-05-03 18:43:42 +0100

    Adding Swift package dependencies in Xcode

--- P16H HotProspects/P16H HotProspects/ContentView.swift
+++ P16H HotProspects/P16H HotProspects/ContentView.swift
@@ -5,12 +5,20 @@
 //  Created by Julian Moorhouse on 03/05/2021.
 //
 
+import SamplePackage
 import SwiftUI
 
 struct ContentView: View {
+    let possibleNumbers = Array(1...60)
+    
+    var results: String {
+        let selected = possibleNumbers.random(7).sorted()
+        let strings = selected.map(String.init)
+        return strings.joined(separator: ",")
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text(results)
     }
 }
 
