2d3a0d59ae9c18727e1bec39104039871da96baa
Julian Moorhouse <julesthecoder@gmail.com>
2021-05-03 11:07:32 +0100

    Initial files for P16E.

---
+++ P16E HotProspects/P16E HotProspects/P16E_HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  P16E_HotProspectsApp.swift
+//  P16E HotProspects
+//
+//  Created by Julian Moorhouse on 03/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct P16E_HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
