80e3b05aaa39260a06417200c9ff7f4d2a2cc3ab
Julian Moorhouse <julesthecoder@gmail.com>
2021-05-03 11:13:53 +0100

    Controlling image interpolation in SwiftUI

--- P16E HotProspects/P16E HotProspects/ContentView.swift
+++ P16E HotProspects/P16E HotProspects/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // This is a small picture
+        Image("example")
+            .interpolation(.none) // now pixelated / looks like line art
+            .resizable()
+            .scaledToFit()
+            .frame(maxHeight: .infinity)
+            .background(Color.black)
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
