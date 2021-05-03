ccb782822acad021533ce486f3a1041eaa1945b2
Julian Moorhouse <julesthecoder@gmail.com>
2021-05-03 17:50:39 +0100

    Initial files for P16G.

---
+++ P16G HotProspects/P16G HotProspects/P16G_HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  P16G_HotProspectsApp.swift
+//  P16G HotProspects
+//
+//  Created by Julian Moorhouse on 03/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct P16G_HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
