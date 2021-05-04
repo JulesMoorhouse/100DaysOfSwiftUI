2a726e3b7d95d7d4fa0468ea86926c4cf0d7dde8
Julian Moorhouse <julesthecoder@gmail.com>
2021-05-04 18:30:45 +0100

    Generating and scaling up a QR code.

--- P16I HotProspects/P16I HotProspects/MeView.swift
+++ P16I HotProspects/P16I HotProspects/MeView.swift
@@ -5,11 +5,52 @@
 //  Created by Julian Moorhouse on 03/05/2021.
 //
 
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct MeView: View {
+    @State private var name = "Anonymous"
+    @State private var emailAddress = "you@yoursite.com"
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+                    .padding(.horizontal)
+                
+                TextField("Email address", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .font(.title)
+                    .padding([.horizontal, .bottom])
+                
+                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                
+                Spacer()
+            }
+            .navigationBarTitle("Your code")
+        }
+    }
+    
+    func generateQRCode(from string: String) -> UIImage {
+        let data = Data(string.utf8)
+        filter.setValue(data, forKey: "inputMessage")
+        
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
+        
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
