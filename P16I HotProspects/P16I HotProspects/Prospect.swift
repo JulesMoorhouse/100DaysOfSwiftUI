dcbd045fac05248e3061a1349d13a3fb39746fb0
Julian Moorhouse <julesthecoder@gmail.com>
2021-05-04 19:19:50 +0100

    Day 84 - Saving and loading data with UserDefaults

--- P16I HotProspects/P16I HotProspects/Prospect.swift
+++ P16I HotProspects/P16I HotProspects/Prospect.swift
@@ -16,14 +16,35 @@ class Prospect: Identifiable, Codable {
 }
 
 class Prospects: ObservableObject {
-    @Published var people: [Prospect]
+    @Published private(set) var people: [Prospect] // private(set) used to stop array being updated outside of this class
+    static let saveKey = "SavedData"
     
     init() {
+        // Notice Self.saveKey has a captial S
+        if let data = UserDefaults.standard.data(forKey: Self.saveKey) {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                self.people = decoded
+                return
+            }
+        }
+        
         self.people = []
+    }
+    
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(people) {
+            UserDefaults.standard.set(encoded, forKey: Self.saveKey)
+        }
+    }
+    
+    func add(_ prospect: Prospect) {
+        people.append(prospect)
+        save()
     }
     
     func toggle(_ prospect: Prospect) {
         objectWillChange.send() // Called before animations are correct
         prospect.isContacted.toggle()
+        save()
     }
 }
