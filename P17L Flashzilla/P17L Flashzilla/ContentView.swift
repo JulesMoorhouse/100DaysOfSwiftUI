df91a3c502153bcf80aee7471aa00d633df3638c
JulesMoorhouse <julesthecoder@gmail.com>
2021-05-06 11:59:46 +0100

    P17L initial files.

---
+++ P17L Flashzilla/P17L Flashzilla/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P17L Flashzilla
+//
+//  Created by Julian Moorhouse on 06/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P17L Flashzilla/P17L Flashzilla/P17L_FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  P17L_FlashzillaApp.swift
+//  P17L Flashzilla
+//
+//  Created by Julian Moorhouse on 06/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct P17L_FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
