316be459fc82f2796d7323b9afcaf9ecdf24e4c2
JulesMoorhouse <julesthecoder@gmail.com>
2021-05-06 11:47:58 +0100

    UIAccessibility.isReduceMotionEnabled.

--- P17K Flashzilla/P17K Flashzilla/ContentView.swift
+++ P17K Flashzilla/P17K Flashzilla/ContentView.swift
@@ -7,11 +7,32 @@
 
 import SwiftUI
 
+// So, when Reduce Motion Enabled is true the closure code that’s passed in is executed immediately, otherwise it’s passed along using withAnimation(). The whole throws/rethrows thing is more advanced Swift, but it’s a direct copy of the function signature for withAnimation() so that the two can be used interchangeably.
+func withOptionalAnimation<Result>( _ animation: Animation? = .default, _ body: () throws -> Result ) rethrows -> Result {
+    if UIAccessibility.isReduceMotionEnabled {
+        return try body()
+    } else {
+        return try withAnimation(animation, body)
+    }
+}
+
 struct ContentView: View {
+    //@Environment(\.accessibilityReduceMotion) var reduceMotion
+    @State private var scale: CGFloat = 1
     
     var body: some View {
         Text("Hello, world!")
-            .padding()
+            .scaleEffect(scale)
+            .onTapGesture {
+//                if self.reduceMotion {
+//                    self.scale *= 1.5
+//                } else {
+//                    withAnimation {
+                    withOptionalAnimation {
+                        self.scale *= 1.5
+                    }
+                //}
+            }
     }
 }
 
