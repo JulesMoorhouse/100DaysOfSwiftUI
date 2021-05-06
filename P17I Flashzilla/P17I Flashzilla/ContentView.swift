e16a345fab2a4201e7fc5dcf7ade3cd9e2e3eb2b
JulesMoorhouse <julesthecoder@gmail.com>
2021-05-06 11:04:46 +0100

    How to be notified when your SwiftUI app moves to the background.

--- P17I Flashzilla/P17I Flashzilla/ContentView.swift
+++ P17I Flashzilla/P17I Flashzilla/ContentView.swift
@@ -10,7 +10,22 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .padding()
+            .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+                print("Moving to the background")
+            }
+            .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+                print("Moving to the foreground")
+            }
+            .onReceive(NotificationCenter.default.publisher(for: UIApplication.userDidTakeScreenshotNotification)) { _ in
+                print("User took a screenshot")
+            }
+            .onReceive(NotificationCenter.default.publisher(for: UIApplication.significantTimeChangeNotification)) { _ in
+                print("user changes their clock or when daylight savings time changes")
+            }
+            .onReceive(NotificationCenter.default.publisher(for: UIApplication.keyboardDidShowNotification)) { _ in
+                print("keyboard is shown")
+            }
+        // Plus many more
     }
 }
 
