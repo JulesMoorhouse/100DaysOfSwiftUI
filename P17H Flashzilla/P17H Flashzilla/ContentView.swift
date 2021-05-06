513fb37aa301316ab98e09448560a4f821e7d5b2
JulesMoorhouse <julesthecoder@gmail.com>
2021-05-06 10:34:31 +0100

    Triggering events repeatedly using a timer.

--- P17H Flashzilla/P17H Flashzilla/ContentView.swift
+++ P17H Flashzilla/P17H Flashzilla/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Every second, but system makes no guarantee it will execute precisely
+   //let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    // Tolerance allows the device to perform important energy optimation with the tolerance amount.
+    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
+    
+    @State private var counter = 0
+    
     var body: some View {
         Text("Hello, world!")
-            .padding()
+            .onReceive(timer) { time in
+                //print("The time is now \(time)")
+                if self.counter == 5 {
+                    self.timer.upstream.connect().cancel()
+                } else {
+                    // writes time 5 times
+                    print("The time is now \(time)")
+                }
+                
+                self.counter += 1
+            }
     }
 }
 
