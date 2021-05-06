dda57850d26114f8c962da9496d63593bb74f278
JulesMoorhouse <julesthecoder@gmail.com>
2021-05-06 07:10:08 +0100

    Initial files for P17.

---
+++ P17 Flashzilla/P17 Flashzilla/P17_FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  P17_FlashzillaApp.swift
+//  P17 Flashzilla
+//
+//  Created by Julian Moorhouse on 06/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct P17_FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ P17 Flashzilla/README.md
@@ -0,0 +1,1 @@
+# Day 86 | [HWS 86](https://www.hackingwithswift.com/100/swiftui/86) | [Index](https://github.com/JulesMoorhouse/100DaysOfSwiftUI/blob/main/README.md)
