cd16fe977f5e10e87c87e6db43c6e8f9cd7172ad
JulesMoorhouse <julesthecoder@gmail.com>
2021-05-06 11:18:23 +0100

    Initial files for P17J.

---
+++ P17J Flashzilla/P17J Flashzilla/P17J_FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  P17J_FlashzillaApp.swift
+//  P17J Flashzilla
+//
+//  Created by Julian Moorhouse on 06/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct P17J_FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
