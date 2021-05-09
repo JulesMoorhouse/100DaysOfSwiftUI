cba803ee2d41e57d14b500ca535005d83e6a6427
Julian Moorhouse <julesthecoder@gmail.com>
2021-05-09 13:07:19 +0100

    Counting down with a Timer.

--- P17M Flashzilla/P17M Flashzilla/ContentView.swift
+++ P17M Flashzilla/P17M Flashzilla/ContentView.swift
@@ -17,8 +17,12 @@ extension View {
 
 struct ContentView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    
     @State private var cards = [Card](repeating: Card.example, count: 10)
-
+    @State private var isActive = true
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         ZStack {
             Image("background")
@@ -27,6 +31,16 @@ struct ContentView: View {
                 .edgesIgnoringSafeArea(.all)
 
             VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(
+                        Capsule()
+                            .fill(Color.black)
+                            .opacity(0.75)
+                    )
                 ZStack {
                     ForEach(0 ..< cards.count, id: \.self) { index in
                         CardView(card: self.cards[index]) {
@@ -59,6 +73,19 @@ struct ContentView: View {
                     .padding()
                 }
             }
+        }
+        .onReceive(timer) { time in
+            guard self.isActive else { return }
+            
+            if self.timeRemaining > 0 {
+                self.timeRemaining -= 1
+            }
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+            self.isActive = false
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+            self.isActive = true
         }
     }
 
