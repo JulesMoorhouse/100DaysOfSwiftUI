40889b4eb1806b62411995ee495bd63e3fd568da
Julian Moorhouse <julesthecoder@gmail.com>
2021-05-09 20:44:16 +0100

    Adding and deleting cards.

--- P17M Flashzilla/P17M Flashzilla/Card.swift
+++ P17M Flashzilla/P17M Flashzilla/Card.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Card {
+struct Card: Codable {
     let prompt: String
     var answer: String
     
--- P17M Flashzilla/P17M Flashzilla/ContentView.swift
+++ P17M Flashzilla/P17M Flashzilla/ContentView.swift
@@ -19,10 +19,13 @@ struct ContentView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     @Environment(\.accessibilityEnabled) var accessibilityEnabled
 
-    @State private var cards = [Card](repeating: Card.example, count: 10)
+    //@State private var cards = [Card](repeating: Card.example, count: 10)
+    @State private var cards = [Card]()
     @State private var isActive = true
     @State private var timeRemaining = 100
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    @State private var showingEditScreen = false
     
     var body: some View {
         ZStack {
@@ -67,6 +70,26 @@ struct ContentView: View {
                 }
             }
 
+            VStack {
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {
+                        self.showingEditScreen = true
+                    }) {
+                        Image(systemName: "plus.circle")
+                            .padding()
+                            .background(Color.black.opacity(0.75))
+                            .clipShape(Circle())
+                    }
+                }
+                
+                Spacer()
+            }
+            .foregroundColor(.white)
+            .font(.largeTitle)
+            .padding()
+            
             if differentiateWithoutColor || accessibilityEnabled {
                 VStack {
                     Spacer()
@@ -121,6 +144,10 @@ struct ContentView: View {
                 self.isActive = true
             }
         }
+        .sheet(isPresented: $showingEditScreen, onDismiss: resetCards) {
+            EditCards()
+        }
+        .onAppear(perform: resetCards)
     }
 
     func removeCard(at index: Int) {
@@ -134,9 +161,18 @@ struct ContentView: View {
     }
     
     func resetCards() {
-        cards = [Card](repeating: Card.example, count: 10)
+        //cards = [Card](repeating: Card.example, count: 10)
         timeRemaining = 100
         isActive = true
+        loadData()
+    }
+    
+    func loadData() {
+        if let data = UserDefaults.standard.data(forKey: "Cards") {
+            if let decoded = try? JSONDecoder().decode([Card].self, from: data) {
+                self.cards = decoded
+            }
+        }
     }
 }
 
