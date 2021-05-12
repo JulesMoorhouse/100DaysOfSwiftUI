4984dd52b31382fdd174a8a2960e66a5db45e7cf
Julian Moorhouse <julesthecoder@gmail.com>
2021-05-12 21:56:26 +0100

    Initial files for P18G.

---
+++ P18G LayoutAndGeometry/P18G LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P18G LayoutAndGeometry
+//
+//  Created by Julian Moorhouse on 12/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P18G LayoutAndGeometry/P18G LayoutAndGeometry/P18G_LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  P18G_LayoutAndGeometryApp.swift
+//  P18G LayoutAndGeometry
+//
+//  Created by Julian Moorhouse on 12/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct P18G_LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
