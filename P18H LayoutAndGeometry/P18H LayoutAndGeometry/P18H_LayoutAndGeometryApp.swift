72010d6e7d71cbf5cd300228c7ec577dbef1f489
Julian Moorhouse <julesthecoder@gmail.com>
2021-05-12 22:05:44 +0100

    P18H initial files.

---
+++ P18H LayoutAndGeometry/P18H LayoutAndGeometry/P18H_LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  P18H_LayoutAndGeometryApp.swift
+//  P18H LayoutAndGeometry
+//
+//  Created by Julian Moorhouse on 12/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct P18H_LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
