889690b2ef67ef66091a5d5e96b01adc244a5256
Julian Moorhouse <julesthecoder@gmail.com>
2021-05-13 21:29:54 +0100

    P18H.

--- P18H LayoutAndGeometry/P18H LayoutAndGeometry/ContentView.swift
+++ P18H LayoutAndGeometry/P18H LayoutAndGeometry/ContentView.swift
@@ -2,15 +2,24 @@
 //  ContentView.swift
 //  P18H LayoutAndGeometry
 //
-//  Created by Julian Moorhouse on 12/05/2021.
+//  Created by Julian Moorhouse on 13/05/2021.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            GeometryReader { geo in
+                Text("Hello, World!")
+                    .frame(width: geo.size.width * 0.9, height: 40)
+                    .background(Color.red)
+            }
+            .background(Color.green)
+
+            Text("More text")
+               .background(Color.blue)
+        }
     }
 }
 
