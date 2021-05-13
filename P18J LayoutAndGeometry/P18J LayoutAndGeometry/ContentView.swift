adc132e46a33cbbb5910d2e98a077475c69089f0
Julian Moorhouse <julesthecoder@gmail.com>
2021-05-13 22:08:16 +0100

    Initial files for P18J.

---
+++ P18J LayoutAndGeometry/P18J LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P18J LayoutAndGeometry
+//
+//  Created by Julian Moorhouse on 13/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
