3d8d80f1e3d8e3c45775c66272507f58f70c1b21
Julian Moorhouse <julesthecoder@gmail.com>
2021-05-19 22:11:21 +0100

    P19A.

---
+++ P19A SnowSeeker/P19A SnowSeeker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P19A SnowSeeker
+//
+//  Created by Julian Moorhouse on 19/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
