ec2822de9d74a299c04727f146bfedbf18349ea5
Julian Moorhouse <julesthecoder@gmail.com>
2021-05-20 19:28:07 +0100

    Making NavigationView work in landscape

--- P19C SnowSeeker/P19C SnowSeeker/ContentView.swift
+++ P19C SnowSeeker/P19C SnowSeeker/ContentView.swift
@@ -35,6 +35,19 @@ struct ContentView: View {
                 }
             }
             .navigationBarTitle("Resorts")
+            
+            WelcomeView()
+        }
+        //.phoneOnlyStackNavigationView()
+    }
+}
+
+extension View {
+    func phoneOnlyStackNavigationView() -> some View {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return AnyView(self.navigationViewStyle(StackNavigationViewStyle()))
+        }  else {
+           return AnyView(self)
         }
     }
 }
