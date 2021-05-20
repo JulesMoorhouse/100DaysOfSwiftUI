b05ffd376f3950fddc57c3198fb4d0b1e03e93b9
Julian Moorhouse <julesthecoder@gmail.com>
2021-05-20 19:53:02 +0100

    Creating a secondary view for NavigationView

---
+++ P19C SnowSeeker/P19C SnowSeeker/ResortView.swift
@@ -0,0 +1,51 @@
+//
+//  ResortView.swift
+//  P19C SnowSeeker
+//
+//  Created by Julian Moorhouse on 20/05/2021.
+//
+
+import SwiftUI
+
+struct ResortView: View {
+    let resort: Resort
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+                Image(decorative: resort.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Group {
+                    HStack {
+                        Spacer()
+                        ResortDetailsView(resort: resort)
+                        SkiDetailsView(resort: resort)
+                        Spacer()
+                    }
+                    .font(.headline)
+                    .foregroundColor(.secondary)
+                    .padding(.top)
+                    
+                    Text(resort.description)
+                        .padding(.vertical)
+                    
+                    Text("Facilities")
+                        .font(.headline)
+                    
+                    Text(ListFormatter.localizedString(byJoining: resort.facilities))
+                        .padding(.vertical)
+                }
+                .padding(.horizontal)
+            }
+        }
+        .navigationBarTitle(Text("\(resort.name), \(resort.country)"), displayMode: .inline)
+    }
+}
+
+struct ResortView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResortView(resort: Resort.example)
+    }
+}
---
+++ P19C SnowSeeker/P19C SnowSeeker/SkiDetailsView.swift
@@ -0,0 +1,25 @@
+//
+//  SkiDetailsView.swift
+//  P19C SnowSeeker
+//
+//  Created by Julian Moorhouse on 20/05/2021.
+//
+
+import SwiftUI
+
+struct SkiDetailsView: View {
+    let resort: Resort
+    
+    var body: some View {
+        VStack {
+            Text("Elevation: \(resort.elevation)m")
+            Text("Snow: \(resort.snowDepth)cm")
+        }
+    }
+}
+
+struct SkiDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SkiDetailsView(resort: Resort.example)
+    }
+}
