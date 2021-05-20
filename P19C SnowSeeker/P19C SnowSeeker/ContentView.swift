485daf50e4794dd52f7ed93bb5b90511618a185e
Julian Moorhouse <julesthecoder@gmail.com>
2021-05-20 18:47:52 +0100

    P19C initial files.

---
+++ P19C SnowSeeker/P19C SnowSeeker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P19C SnowSeeker
+//
+//  Created by Julian Moorhouse on 20/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P19C SnowSeeker/README.md
@@ -0,0 +1,5 @@
+# Day 97 | [HWS 97](https://www.hackingwithswift.com/100/swiftui/97) | [Index](https://github.com/JulesMoorhouse/100DaysOfSwiftUI/blob/main/README.md)
+
+- [ContentView.swift](https://github.com/JulesMoorhouse/100DaysOfSwiftUI/blob/main/P19C%20SnowSeeker/P19C%20SnowSeeker/ContentView.swift)
+
+- Building a primary list of items.
