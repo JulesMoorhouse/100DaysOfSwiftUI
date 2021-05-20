1f40b2d190ad7492e6cd029c79737cb21fbde8ec
Julian Moorhouse <julesthecoder@gmail.com>
2021-05-20 19:17:29 +0100

    Building a primary list of items

---
+++ P19C SnowSeeker/P19C SnowSeeker/Bundle-Decodeable.swift
@@ -0,0 +1,28 @@
+//
+//  Bundle-Decodeable.swift
+//  P19C SnowSeeker
+//
+//  Created by Julian Moorhouse on 20/05/2021.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode<T: Decodable>(_ file: String) -> T {
+        guard let url = self.url(forResource: file, withExtension: nil) else  {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+        
+        return loaded
+    }
+}
